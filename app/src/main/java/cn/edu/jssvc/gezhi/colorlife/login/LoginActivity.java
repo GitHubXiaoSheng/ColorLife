7c05c3dd4dde828c45f44c2f6e7ced9822029592
mayunxiaosheng <2438955217@qq.com>
2019-04-08 20:50:50 +0800

    zl

--- app/src/main/java/cn/edu/jssvc/gezhi/colorlife/login/LoginActivity.java
+++ app/src/main/java/cn/edu/jssvc/gezhi/colorlife/login/LoginActivity.java
@@ -152,6 +152,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+
     //    private void displayImage(String imagePath) {
 //        if (imagePath != null) {
 ////            Bitmap bitmap = BitmapFactory.decodeFile( imagePath );
